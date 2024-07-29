717ea21345bfab9b80080f6545a909c04b19d419
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-29 21:51:30 +0530

    adding custom css and configuring the tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,27 @@
-import "@/styles/globals.css";
 import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
 
 import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "LiveDocs",
+  description: "Your go-to colabarator editor",
+};
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
-export default function RootLayout({ children }: [children: ReactNode]) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
+        className={cn("min-h-screen font-sans antialiased", fontSans.variable)}
       >
-        ...
+        {children}
       </body>
     </html>
   );
