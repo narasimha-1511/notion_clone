99af048a88e3f6627e68d45c9bc234df59dbabef
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-31 03:02:54 +0530

    adding the live block to our project

---
+++ app/Provider.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  LiveblocksProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+      {/* <RoomProvider id="my-room"> */}
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        {children}
+      </ClientSideSuspense>
+      {/* </RoomProvider> */}
+    </LiveblocksProvider>
+  );
+};
+
+export default Provider;
