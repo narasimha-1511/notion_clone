522fe4706a87cc5aa3b1cec2e615c8e564e73afc
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-31 14:09:29 +0530

    addings a action to create room for realtime updates

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,34 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
+import Header from "@/components/Header";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 const Home = () => {
+  const documents = [];
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+      {documents.length > 0 ? (
+        <div></div>
+      ) : (
+        <div className="document-list-empty">
+          <Image
+            src="/assets/icons/doc.svg"
+            alt="Document"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+        </div>
+      )}
+    </main>
   );
 };
 
--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -1,3 +1,4 @@
+import { getUserColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
 import { Liveblocks } from "@liveblocks/node";
 import { redirect } from "next/navigation";
@@ -20,20 +21,21 @@ export async function POST(request: Request) {
   const user = {
     id: id,
     info: {
-      firstName,
-      lastName,
+      id: id,
+      name: `${firstName} ${lastName}`,
       email: emailAddresses[0].emailAddress,
-      imageUrl,
+      avatar: imageUrl,
+      color: getUserColor(id),
     },
   };
 
   // Identify the user and return the result
   const { status, body } = await liveblocks.identifyUser(
     {
-      userId: user.id,
-      groupIds, // Optional
+      userId: user.info.email,
+      groupIds: [],
     },
-    { userInfo: user.metadata }
+    { userInfo: user.info }
   );
 
   return new Response(body, { status });
